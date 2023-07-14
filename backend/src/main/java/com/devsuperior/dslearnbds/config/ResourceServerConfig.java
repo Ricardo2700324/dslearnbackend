d7b597f16d6a584a649bd9618c7e2d6c4566c3c3
Ricardo Fernando de Souza <rfs.br@hotmail.com>
2023-07-14 11:47:51 -0300

    Autorização customizada em nivel de serviço parte final

--- backend/src/main/java/com/devsuperior/dslearnbds/config/ResourceServerConfig.java
+++ backend/src/main/java/com/devsuperior/dslearnbds/config/ResourceServerConfig.java
@@ -24,9 +24,6 @@ public class ResourceServerConfig extends ResourceServerConfigurerAdapter{
 	private JwtTokenStore tokenStore;
 	
 	private static final String[] PUBLIC = { "/oauth/token", "/h2-console/**" };
-	private static final String[] OPERATOR_OR_ADMIN = { "/products/**", "/categories/**" };
-	private static final String[] ADMIN = { "/users/**" };
-	
 
 	
 	@Override
@@ -44,9 +41,6 @@ public class ResourceServerConfig extends ResourceServerConfigurerAdapter{
 		
 		http.authorizeRequests()
 		.antMatchers(PUBLIC).permitAll()
-		.antMatchers(HttpMethod.GET, OPERATOR_OR_ADMIN).permitAll()
-		.antMatchers(OPERATOR_OR_ADMIN).hasAnyRole("OPERATOR", "ADMIN")
-		.antMatchers(ADMIN).hasAnyRole("ADMIN")
 		.anyRequest().authenticated();
 	
 	}
